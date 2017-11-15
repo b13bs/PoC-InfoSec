205e73d8f9e1f52a01e4ca0831155b51a31c577e
b13bs <etienne.ducharme@gmail.com>
2017-11-15 14:23:36 -0500

    RSA implementation

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,78 +1,82 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-import sys
 import random
+from pprint import pprint
+import os
+import codecs
+import fractions
+import binascii
+import json
+
+def phi(n):
+    amount = 0
+
+    for k in range(1, n + 1):
+        if fractions.gcd(n, k) == 1:
+            amount += 1
+
+    return amount
 
 
-
-def egcd(a, b):
-	x,y, u,v = 0,1, 1,0
-	while a != 0:
-		q, r = b//a, b%a
-		m, n = x-u*q, y-v*q
-		b,a, x,y, u,v = a,r, u,v, m,n
-	gcd = b
-	return gcd, x, y
-
-def modinv(a, m):
-	gcd, x, y = egcd(a, m)
-	if gcd != 1:
-		raise Exception("Modular inverse does not exist")
-	else:
-		return x % m
+with open("primes.txt") as f:
+    lines = f.readlines()
+    p1 = int(random.choice(lines).strip())
+    p2 = int(random.choice(lines).strip())
 
 
-with open("primes.txt", "r") as f:
-	primes = f.read().split()
+def generate_keys():
+    n = p1*p2
+    phi_n = (p1-1) * (p2-1)
 
-# P
-p = int(random.choice(primes))
+    # choisir e, prime relative à phi_n (phi_n peut pas être divisable par e)
+    for i in range(100):
+        e = i+2
+        if not (phi_n/e).is_integer():
+            break
 
-# Q
-different = False
-while not different:
-	q = int(random.choice(primes))
-	if q == p:
-		different = False
-	else:
-		different = True
+    # computer d
+    for i in range(100):
+        k = i+1+1
+        d = (k * phi_n + 1)/e
+        if d.is_integer():
+            d = int(d)
+            break
 
-# N et phi_N
-n = p*q
-phi_n = (p - 1) * (q - 1)
+    keys = {"p1": p1, "p2": p2, "n": n, "phi_n": phi_n, "e": e, "k": k, "d": d}
+    json.dump(keys, open("keys", "w"))
 
-# E
-if phi_n > 65537:
-	e = 65537
-elif phi_n > 257:
-	e = 257
-elif phi_n > 17:
-	e = 17
-elif phi_n > 5:
-	e = 5
-else:
-	e = 3
+    return (n, e, d)
 
-# D
-d = modinv(e, phi_n)
+def load_keys():
+    keys = json.load(open("keys"))
+    return (keys["n"], keys["e"], keys["d"])
 
-print("p: %s" % p)
-print("q: %s" % q)
-print("d: %s" % d)
-print("e: %s" % e)
-print("phi_n: %s" % phi_n)
-print("n: %s" % n)
+def encrypt(msg, e, n):
+    return (msg**e) % n
 
-m = 133713
-print("m: %s" % m)
+def string_to_int(chaine):
+    return int(codecs.encode(chaine.encode(), "hex"), 16)
 
-ciphered = (m**e) % n
-print("ciphered: %s" % ciphered)
+def int_to_string(integer):
+    return codecs.decode(hex(integer)[2:].encode(), "hex").decode()
 
-decrypted = (ciphered**d) % n
-print("decrypted: %s" % decrypted)
+def decrypt(encrypted_msg, d, n):
+    return (encrypted_msg**d) % n
 
-if decrypted == m:
-	print("+++ TOUT FONCTIONNE!")
-else:
-	print("--- désolé...")
+
+if __name__ == "__main__":
+    if not os.path.isfile("keys"):
+        n, e, d = generate_keys()
+    else:
+        n, e, d = load_keys()
+
+    print("n: %s\ne: %s\nd: %s" % (n,e,d))
+
+    SECRET = 5555
+    print("secret: %s" % SECRET)
+    encrypted_msg = encrypt(SECRET, e, n)
+    print("encrypted_msg: %s" % encrypted_msg)
+    decrypted_msg = decrypt(encrypted_msg, d, n)
+    print("decrypted_msg: %s" % decrypted_msg)
+
+    assert (SECRET == decrypted_msg)
